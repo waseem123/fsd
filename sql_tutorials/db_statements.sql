000dd1c58eed3479501e6029bc6c49daaf042aeb
WASEEM ATTAR <waseem_6957@yahoo.com>
2023-06-05 12:27:48 +0530

    Add files via upload

---
+++ sql_tutorials/db_statements.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE db_findmyjob;
+USE db_findmyjob;
+-- DDL
+-- 1.CREATE
+-- 2.ALTER
+-- 3.DROP
+
+-- DML
+-- 1.INSERT
+-- 2.UPDATE
+-- 3.DELETE
+
+-- DQL
+-- 1.SELECT
+
+-- DCL
+-- 1.GRANT
+-- 2.REVOKE
